6495f0d4d84f44386b0b10beeec2410a1b02685d
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-10-20 05:27:24 +0530

    20th October

--- Arrays/Intermediate-Question/product_Array_exceptSelf.java
+++ Arrays/Intermediate-Question/product_Array_exceptSelf.java
@@ -1,21 +1,24 @@
+// Find the Prefix and suffix of all nums
 public class product_Array_exceptSelf{
     public static int[] productExceptSelf(int[] nums) {
-        int avoid = 0;
-        int new_arr[] = new int[nums.length];
-        while ( avoid != nums.length ) {
-            int currProd = 1;
-            for( int i = 0 ; i < nums.length ; i++ ){
-                if( nums[i] == nums[avoid] ) continue;
-                currProd *= nums[i];
-            }
-            System.out.println(currProd);
-            new_arr[avoid] = currProd;
-            avoid++;
-        }
-        return new_arr;
+        // Method to find Prefix
+        int prefix[] = new int[nums.length];
+        prefix[0] = 1;
+        for( int i = 1 ; i < nums.length ; i++ ) prefix[i] = nums[i-1]*prefix[i-1];
+        
+        // Method to find Suffix
+        int suffix[] = new int[nums.length];
+        suffix[nums.length-1] = 1;
+        for( int i = nums.length-2 ; i >= 0 ; i-- )suffix[i] = suffix[i+1]*nums[i+1];
+
+        // Calculate Final ans
+        for( int i = 0; i < nums.length ; i++ ) nums[i] = prefix[i]*suffix[i];
+        
+        return nums;
     }
+    
     public static void main(String[] args) {
-        int arr[] = { 1 , 2 , 3 , 4 };
+        int arr[] = { 1 , 2 , 3 , 4 , 5 };
         productExceptSelf( arr );
     } 
 }
---
+++ Arrays/Intermediate-Question/product_array_exceptSelf_optimum.java
@@ -0,0 +1,29 @@
+// Find Prefix and suffix then mulitply their respective values
+public class product_array_exceptSelf_optimum {
+    public static int[] productExceptSelf(int[] nums) {
+        
+        int answer [] = new int[nums.length];
+        
+        for( int i = 0 ; i < nums.length ; i++ ){
+            if( i == 0 ){
+                nums[i] = 1;
+                answer[i] = 1;
+                continue;
+            }
+            answer[i]=answer[i-1] * nums[i-1];
+        }
+        int suffix = 1;
+        for( int j = nums.length-1 ; j >= 0 ; j-- ){
+            if( j == nums.length-1 ){
+                continue;
+            }
+            suffix *= nums[j+1];
+            answer[j] *= suffix;
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        int arr[] = { 1 , 2 , 3 , 4 , 5 };
+        productExceptSelf( arr );
+    } 
+}
