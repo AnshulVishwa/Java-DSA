8a7575d3a4a27159821dc4efe6d2e9c9253abc33
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-10-20 16:41:02 +0530

    20th October

---
+++ Arrays/Hard Questions/buy_sell_III.java
@@ -0,0 +1,9 @@
+public class buy_sell_III {
+    public static int maxProfit(int[] prices) {
+        
+    }
+    public static void main(String[] args) {
+        int arr[] = { 3,3,5,0,0,3,1,4 };
+        System.out.println("profit : " + maxProfit(arr));
+    }
+}
