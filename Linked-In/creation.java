9d190c481ad9c36f83a79dd35f6a6e205f49b4ad
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-10-20 16:41:28 +0530

    20th October

--- Linked-In/creation.java
+++ Linked-In/creation.java
@@ -1,18 +1,28 @@
+// Creating a node which consist of data and ref. of next node
 class Node{
-    String name;
-    int roll;
+    int data;
     Node next;
-    public Node( String name , int roll ){
-        this.name = name;
-        this.roll = roll;
+    // Constructor of Node which includes data.
+    public Node( int data ){
+        // Initializing data with the value.
+        this.data = data;
+        // Initializing next with the null cuz Node.next -> null.
+        this.next = null;
     }
 }
+// Linked List class which connects all the NODES
 class LinkedList{
-    private:
-        Node head = new Node();
-        head -> next = null;
-    LinkedList()
+    // head node Starting point
+    Node head;
+    // Constructor for initializing head to the null when the list is created.
+    public LinkedList () {
+        head = null;
+    }
 }
-public class creation {
-        
+// Main Class
+public class creation{
+    public static void main(String[] args) {
+        // Creating a Linked List
+        LinkedList list = new LinkedList();
+    }
 }
