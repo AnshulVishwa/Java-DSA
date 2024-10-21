16d000652bbc8ad9ef4a212798cb7cca3b571389
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-10-21 21:32:24 +0530

    21st October

--- Linked-List/insertFirst.java
+++ Linked-List/insertFirst.java
@@ -1,3 +1,27 @@
+class Node{
+    int data;
+    Node next;
+    public Node( int data ){
+        this.data = data;
+        this.next = null;
+    }
+}
+class LinkedList{
+    Node head;
+    LinkedList(){
+        head = null ;
+    }
+    void insertFirst( int data ){
+        Node newNode = new Node(data);
+        if( head == null ){
+            head = newNode;
+        }
+        else{
+            Node temp = head.next;
+            head = temp;
+        }
+    }
+}
 public class insertFirst {
     public static void main(String[] args) {
         
