e31b13ee898740d88f277b0c173a299d3bad6948
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-11-14 16:18:22 +0530

    Adding some imp questions

---
+++ Other_Question/average_of_array.java
@@ -0,0 +1,19 @@
+// To develop a program to find an average of an array using AVG function.
+
+import java.util.Random;
+
+public class average_of_array {
+    public static void main(String[] args) {
+        int arr[] = new int[10];
+        for( int i = 0 ; i < 10 ; i++ ){
+            Random rand = new Random();
+            arr[i] = rand.nextInt(10);
+            System.out.print(arr[i] + "\t");
+        }
+        System.out.println();
+        double sum = 0;
+        for( int i = 0 ; i < arr.length ; i++ ) sum += arr[i];
+        sum = sum/10;
+        System.out.println("Average of the array is : " + sum );
+    }
+}
