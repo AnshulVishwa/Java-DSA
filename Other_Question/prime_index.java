df7afb09193c454083a4400f5d5950f071c1440c
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-11-14 21:11:21 +0530

    14th Nov

--- Other_Question/prime_index.java
+++ Other_Question/prime_index.java
@@ -1,7 +1,7 @@
 // To develop a program to print all the values whose index is prime in an array
 public class prime_index {
     public static void main(String[] args) {
-        int arr[] = {1,5,8,5,8,5,9,6,2,3,5,4,7,8,5,9,6,2,2,5};
+        int arr[] = {1,5,8,5,8,5,9,6,2,3,5,4,7,8,5,9,6,2,2};
         for( int j = 1 ; j < arr.length ; j++ ){
             int half = j/2;
             boolean flag = false;
