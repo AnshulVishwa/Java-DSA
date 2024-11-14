3dd3d5a3833e8e13a67e38671505584aeddad690
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-11-14 20:41:27 +0530

    14th November

---
+++ Other_Question/prime_index.java
@@ -0,0 +1,16 @@
+public class prime_index {
+    public static void main(String[] args) {
+        int arr[] = {1,5,8,5,8,5,9,6,2,3,5,4,7,8,5,9,6,2,2,5};
+        for( int j = 1 ; j < arr.length ; j++ ){
+            int half = j/2;
+            boolean flag = false;
+            for( int i = 2 ; i <= half ; i++ ){
+                if( j%i == 0 ) {
+                    flag = true;
+                    break;
+                }
+            }
+            if( !flag ) System.out.println("Primium Value : " + arr[j]);
+        }
+    }
+}
