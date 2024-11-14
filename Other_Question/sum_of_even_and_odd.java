c24347030af5c18e3fcb83de61a550f3c71178c0
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-11-14 17:12:05 +0530

    14th November

--- Other_Question/sum_of_even_and_odd.java
+++ Other_Question/sum_of_even_and_odd.java
@@ -1,3 +1,5 @@
+// Write a program to calculate sum of even and odd numbers in an array
+
 public class sum_of_even_and_odd {
     public static void main(String[] args) {
         int arr[] = { 1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 };
