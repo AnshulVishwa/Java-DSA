9dff2b9f261a0bc71d66caef6ec2bf216da3f387
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-11-16 07:35:06 +0530

    Sorting revision

---
+++ Other_Question/Revision Sorting/selectionSort.java
@@ -0,0 +1,23 @@
+public class selectionSort {
+    public static void sort(int arr[]) {
+        for( int i = 0 ; i < arr.length-1 ; i++ ){
+            int min = i;
+            for( int j = i+1 ; j < arr.length ; j++ ) if( arr[j] < arr[min] ) min = j;
+            swap(arr, min, i);
+        }
+    }
+    public static void swap( int arr[] , int i , int j ){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void display( int arr[] ){
+        for( int i = 0; i  < arr.length ; i++ ) System.out.print( arr[i] + "\t" );
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[] = { 38, 13, 90, 96, 33, 69, 25, 98, 11, 23, 53, 10 };
+        sort(arr);
+        display(arr);
+    }
+}
