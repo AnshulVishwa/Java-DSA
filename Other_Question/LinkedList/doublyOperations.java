5efe184db9a8248cbae6775ebc76534fc6569573
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-11-24 08:08:07 +0530

    Reversing list

---
+++ Other_Question/LinkedList/doublyOperations.java
@@ -0,0 +1,66 @@
+class Node{
+    Node prev;
+    int data;
+    Node next;
+    Node( int val ){
+        prev = null;
+        data = val;
+        next = null;
+    }
+}
+class LinkedList{
+    Node head;
+    LinkedList() {head = null;}
+    void insert( int data , int where){
+        Node newNode = new Node(data);
+        if( head == null) head = newNode;
+        else{
+            Node temp = head;
+            while( temp.data != where ) temp = temp.next;
+            newNode.next = temp.next;
+            newNode.prev = temp;
+            if (temp.next != null) {
+                temp.next.prev = newNode;
+            }
+            temp.next = newNode;
+        }
+    }
+    void delete( int var , Node temp ){
+        if( temp.next == null && temp.prev == null ) {
+            head = null;
+            return;
+        }
+        while( temp.data != var ){
+            if( temp.next == null ){
+                System.out.println("No value found");
+                return;
+            }
+            temp = temp.next;
+        }
+        if( temp.next == null ) {
+            temp.prev.next = null;
+        }
+        else{
+            temp.prev.next = temp.next;
+            temp = null;
+        }
+    }
+    void display( Node temp ){
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+}
+public class doublyOperations {
+        public static void main(String[] args) {
+            LinkedList list = new LinkedList();
+            list.insert(10,0);
+            list.insert(20,10);
+            list.insert(30,20);
+            list.insert(40,30);
+            list.delete(20,list.head);
+            list.display(list.head);
+        }
+    }
--- Other_Question/LinkedList/reverse.java
+++ Other_Question/LinkedList/reverse.java
@@ -23,6 +23,7 @@ public class reverse {
         Node head = new Node(10);
         head.next = new Node(20);
         head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
 
         head = reverseList(head);
 
