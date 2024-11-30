dd8df43e5b15b293c3235304e04d669dbe3acc0e
AnshulVishwa <anshulvishwa2205@gmail.com>
2024-11-30 20:57:07 +0530

    validArrange

---
+++ Strings/Intermediate Questions/validArrang.java
@@ -0,0 +1,20 @@
+public class validArrang {
+    public static int[][] validArrangement(int[][] pairs) {
+        int arr[][] = new int[pairs.length][pairs[0].length];
+        int index1 = 0;
+        int index2 = 0;
+        for( int i = 0 ; i < pairs.length ; i++ ){
+            int first = arr[i][0];
+            int last = arr[i][1];
+            for( int j = 0 ; j < pairs.length ; j++ ){
+                if( i == j ) continue;
+                if( pairs[i][j] == first ) arr[index1++][index2++] = ;
+            }
+        }
+        return pairs;
+    }
+    public static void main(String[] args) {
+        int arr[][] = { { 1 , 2 } , { 3 , 4 } , { 2 , 3 } , { 4 , 5 } };
+        validArrangement(arr);
+    }
+}
