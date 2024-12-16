ad7f53ac975735d978c2857e615f2da15e591a4a
Anshul Vishwakarma <165347635+AnshulVishwa@users.noreply.github.com>
2024-12-16 16:59:29 +0000

    adding

--- Arrays/Easy Questions/duplicate.java
+++ Arrays/Easy Questions/duplicate.java
@@ -1,30 +1,30 @@
-import java.util.Scanner;
-public class duplicate {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner( System.in );
-        System.out.print("Enter the length of the array : ");
-        int length = sc.nextInt();
+// import java.util.Scanner;
+// public class duplicate {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner( System.in );
+//         System.out.print("Enter the length of the array : ");
+//         int length = sc.nextInt();
 
-        int arr[] = new int[length];
+//         int arr[] = new int[length];
 
-        for( int i = 0 ; i < length ; i++ ){
-            System.out.print("Enter " + (int)(i+1) + " element value : ");
-            arr[i] = sc.nextInt();
-            if( i == length - 1 ) System.out.println("Array is Formed"); 
-        }
+//         for( int i = 0 ; i < length ; i++ ){
+//             System.out.print("Enter " + (int)(i+1) + " element value : ");
+//             arr[i] = sc.nextInt();
+//             if( i == length - 1 ) System.out.println("Array is Formed"); 
+//         }
 
-        boolean say = false;
+//         boolean say = false;
 
-        System.out.println("Checking for duplicate");
+//         System.out.println("Checking for duplicate");
 
-        for( int i = 0 ; i < arr.length ; i++ ){
-            for( int j = 0 ; j < arr.length ; j++ ){
-                if( i == j ) continue;
-                if( arr[i] == arr[j] ) say = true; break; 
-            }
-        }
+//         for( int i = 0 ; i < arr.length ; i++ ){
+//             for( int j = 0 ; j < arr.length ; j++ ){
+//                 if( i == j ) continue;
+//                 if( arr[i] == arr[j] ) say = true; break; 
+//             }
+//         }
 
-        if( say ) System.out.println("There is a duplicate in this array.");
-        else System.out.println("No Duplicate in this array");
-    }
-}
+//         if( say ) System.out.println("There is a duplicate in this array.");
+//         else System.out.println("No Duplicate in this array");
+//     }
+// }
