3b62f6bc6b039c40db665b107873240a3f154d5d
AnshulVishwa <anshulvishwa2205@gmail.com>
2025-03-16 09:15:37 +0530

    Closing scanner

--- Arrays/Hard Questions/first_missing_pos.java
+++ Arrays/Hard Questions/first_missing_pos.java
@@ -1,8 +1,7 @@
-import java.util.Arrays;
-
 public class first_missing_pos {
     public static void main(String[] args) {
         int nums[] = {1,2,0};
+        int n = nums.length;
         for (int i = 0; i < n; i++) {
             while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
                 int temp = nums[nums[i] - 1];
